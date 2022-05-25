58bfa5609e896997689d1bb86dc806f6a552091e
Sangyong-Jeon <tkddyd420@naver.com>
2022-05-25 19:55:51 +0900

    리액트에서 배열 사용하기 3 - 데이터 삭제하기

--- simplediary/src/DiaryList.js
+++ simplediary/src/DiaryList.js
@@ -1,6 +1,6 @@
 import DiaryItem from './DiaryItem';
 
-const DiaryList = ({ diaryList }) => {
+const DiaryList = ({ onDelete, diaryList }) => {
   console.log(diaryList);
   return (
     <div className="DiaryList">
@@ -8,7 +8,7 @@ const DiaryList = ({ diaryList }) => {
       <h4>{diaryList.length}개의 일기가 있습니다.</h4>
       <div>
         {diaryList.map((it, idx) => (
-          <DiaryItem key={it.id} {...it} />
+          <DiaryItem key={it.id} {...it} onDelete={onDelete} />
           // 고유한 id가 있으면 그것을 쓰고, 없다면 배열의 인덱스(idx) 쓰기! 왜냐면 인덱스 순서가 갑자기 바뀌면 리액트에서 문제가 생길 수 있음.
         ))}
       </div>
