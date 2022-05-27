4edc615370b5bf96eedccf4a9210a94d89a9d314
Sangyong-Jeon <tkddyd420@naver.com>
2022-05-27 10:52:42 +0900

    React에서 API 호출하기
    자바스크립트 API 내장 객체인 fetch 사용

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,8 +1,9 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
-import Lifecycle from './Lifecycle';
+// 리액트 라이프사이클 연습용 컴포넌트
+// import Lifecycle from './Lifecycle';
 
 // const dummyList = [
 //   {
@@ -35,10 +36,41 @@ import Lifecycle from './Lifecycle';
 //   },
 // ];
 
+// https://jsonplaceholder.typicode.com/comments
+
 function App() {
   const [data, setData] = useState([]);
 
   const dataId = useRef(0);
+
+  // javascript api 내장 객체인 fetch 사용
+  const getData = async () => {
+    // 비동기 함수로 만듬 async
+    const res = await fetch(
+      'https://jsonplaceholder.typicode.com/comments'
+    ).then((res) => res.json());
+
+    // 배열에 slice(0,20)을 사용하면 0~19까지만 잘라서 가져온다.
+    // map을 쓰면 배열을 forEach문처럼 돌려서 return값을 반환한다.
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        // js의 내장 객체인 수학 연산을 담당하는 Math 객체 사용함.
+        // random() * 5는 0~4까지 랜덤 난수(정수가 아닌 실수)를 발생시킴.
+        // floor()는 실수를 정수로 변환시켜주므로 0~4가 나옴.
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -69,7 +101,7 @@ function App() {
 
   return (
     <div className="App">
-      <Lifecycle />
+      {/* <Lifecycle /> */}
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
