54a7c29ed4d22417e43b644b7da269c8baf3026a
Sangyong-Jeon <tkddyd420@naver.com>
2022-06-26 13:41:06 +0900

    주석제거

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -5,39 +5,6 @@ import DiaryList from './DiaryList';
 // 리액트 라이프사이클 연습용 컴포넌트
 // import Lifecycle from './Lifecycle';
 
-// const dummyList = [
-//   {
-//     id: 1,
-//     author: '홍길동',
-//     content: '하이 1',
-//     emotion: 5,
-//     created_date: new Date().getTime(), // new Date()는 현재 시간 기준으로 생성됨. getTime()은 시간을 밀리세컨드로 변환하여 number 타입으로 반환
-//   },
-//   {
-//     id: 2,
-//     author: '김철수',
-//     content: '하이 2',
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 3,
-//     author: '박짱구',
-//     content: '하이 3',
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 4,
-//     author: '훈이',
-//     content: '하이 4',
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-// ];
-
-// https://jsonplaceholder.typicode.com/comments
-
 const reducer = (state, action) => {
   // 첫번째 파라미터는 상태변화가 일어나기 직전의 상태(state), 두번째는 어떤 상태변화를 일으켜야하는지 정보가 있는 액션 객체
   switch (action.type) {
