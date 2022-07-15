707b590b2fb289c854aca4756a6b59151eeb74dd
Sangyong-Jeon <tkddyd420@naver.com>
2022-07-15 15:36:04 +0900

    프로젝트 기초 공사2
    - 프로젝트 기초 공사 1에서 연습했던 부분을 삭제
    - 전역에서 사용할 변수인 data와 함수들을 정의
    - useReducer, useRef, React.createContext() 사용
    - Provider 태그로 감싸서 value값에 함수 및 변수를 넣는다.

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -1,3 +1,5 @@
+import React, { useReducer, useRef } from 'react';
+
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
@@ -5,10 +7,6 @@ import Home from './pages/Home';
 import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
-
-// COMPONENTS
-import MyButton from './components/MyButton';
-import MyHeader from './components/MyHeader';
 
 /* 페이지 라우팅 1 - React Router 기본
  <BrowserRouter>로 감싸면 브라우저 url과 매핑될 수 있다. ex) /new => <New /> , /edit => <Edit />
@@ -32,43 +30,90 @@ Edit.js파일에서 useNavigate를 사용하여 페이지를 이동시킨다.
 /* 프로젝트 기초 공사 1
 process.env.PUBLIC_URL 은 현제 프로젝트 내에서 public 폴더를 가리키는 경로(url)이다.
 */
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <MyHeader
-          headText={'App'}
-          leftChild={
-            <MyButton text={'왼쪽 버튼'} onClick={() => alert('왼쪽 클릭')} />
-          }
-          rightChild={
-            <MyButton
-              text={'오른쪽 버튼'}
-              onClick={() => alert('오른쪽 클릭')}
-            />
-          }
-        />
-        <h2>App.js</h2>
 
-        <MyButton
-          text={'버튼'}
-          onClick={() => alert('버튼 클릭')}
-          type={'positive'}
-        />
-        <MyButton
-          text={'버튼'}
-          onClick={() => alert('버튼 클릭')}
-          type={'negative'}
-        />
-        <MyButton text={'버튼'} onClick={() => alert('버튼 클릭')} />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/new" element={<New />} />
-          <Route path="/edit" element={<Edit />} />
-          <Route path="/diary/:id" element={<Diary />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case 'INIT': {
+      return action.data;
+    }
+    case 'CRAETE': {
+      newState = [action.data, ...state];
+      break;
+    }
+    case 'REMOVE': {
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
+    }
+    case 'EDIT': {
+      newState = state.map((it) =>
+        it.id === action.data.id ? { ...action.data } : it
+      );
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+};
+
+export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
+
+function App() {
+  const [data, dispatch] = useReducer(reducer, []);
+  const dataId = useRef(0);
+
+  // CREATE
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: 'CREATE',
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+    dataId.current += 1;
+  };
+
+  // REMOVE
+  const onRemove = (targetId) => {
+    dispatch({ type: 'REMOVE', targetId });
+  };
+
+  // EDIT
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: 'EDIT',
+      data: {
+        id: targetId,
+        data: {
+          id: targetId,
+          date: new Date(date).getTime(),
+          content,
+          emotion,
+        },
+      },
+    });
+  };
+
+  return (
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={{ onCreate, onEdit, onRemove }}>
+        <BrowserRouter>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/new" element={<New />} />
+              <Route path="/edit" element={<Edit />} />
+              <Route path="/diary/:id" element={<Diary />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
   );
 }
 
